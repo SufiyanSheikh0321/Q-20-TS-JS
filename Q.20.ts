5d18d98f8e36b1150efc57aa664bd121db21103d
Muhammed Sufiyan <148213204+SufiyanSheikh0321@users.noreply.github.com>
2024-02-29 20:10:31 +0500

    Add files via upload

---
+++ Q.20.ts
@@ -0,0 +1,33 @@
+// 20.Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+
+
+let itemsList = [
+    // Mountains
+    "Mount Everest",
+    "K2",
+    "Denali",
+    
+    // Rivers
+    "Amazon River",
+    "Nile River",
+    "Yangtze River",
+    
+    // Countries
+    "USA",
+    "India",
+    "Brazil",
+    
+    // Cities
+    "Tokyo",
+    "Paris",
+    "New York",
+    
+    // Languages
+    "English",
+    "Spanish",
+    "Mandarin"
+];
+
+
+console.log("Items List:", itemsList);
